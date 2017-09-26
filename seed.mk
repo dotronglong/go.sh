de77300a356132dfd5733e3138a3aaf5a0e6bc79
Long Do <me@dotronglong.com>
2017-09-26 19:13:52 +0700

    Update seed.mk

--- seed.mk
+++ seed.mk
@@ -11,6 +11,8 @@ $(GO_SH)/bin/easyjson:
 	@echo "Installing easyjson ..."
 	@go get github.com/mailru/easyjson/...
 	@mv $(GOPATH)/bin/easyjson $(GO_SH)/bin/easyjson
+clean-easyjson:
+	@find $(APP_DIR)/entity/ -name "*_easyjson.go" -exec rm -rf {} \;	
 
 .PHONY: get-swagger
 get-swagger: $(GO_SH)/bin/swagger
