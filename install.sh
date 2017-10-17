d90cb1a71a3d8089e0360730e0bb7cc48473d00d
Long Do <me@dotronglong.com>
2017-10-17 22:07:09 +0700

    Update install docker

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ docker() {
 	    curl \
 	    gnupg2 \
 	    software-properties-common \
-	&& curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add - \
+	&& curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add - \
 	&& add-apt-repository \
 	 "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
 	 $(lsb_release -cs) \
