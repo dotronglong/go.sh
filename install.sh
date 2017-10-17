3d548147d14d40aaba0d602fc0fa931abc62843f
Long Do <me@dotronglong.com>
2017-10-17 22:25:57 +0700

    Ignore verify fingerprint

--- install.sh
+++ install.sh
@@ -25,7 +25,6 @@ docker() {
 	    curl \
 	    gnupg2 \
 	    software-properties-common \
-	&& curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add - \
 	&& add-apt-repository \
 	 "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
 	 $(lsb_release -cs) \
