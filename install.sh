e5f0493d0dcb9b1d5b60ed406a7ea8bc9ba6fe5f
Long Do <me@dotronglong.com>
2017-10-17 21:56:24 +0700

    Add docker installation

--- install.sh
+++ install.sh
@@ -17,4 +17,24 @@ glide() {
 	$CUR_DIR/print.sh info "Glide is installed at $BIN_DIR/glide"
 }
 
+docker() {
+	apt-get update \
+	&& apt-get install -y \
+	    apt-transport-https \
+	    ca-certificates \
+	    curl \
+	    gnupg2 \
+	    software-properties-common \
+	&& curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add - \
+	&& add-apt-repository \
+	 "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+	 $(lsb_release -cs) \
+	 stable"
+
+    apt-get update \
+	&& apt-get install -y docker-ce
+
+	$CUR_DIR/print.sh info $(docker version)
+}
+
 $*
